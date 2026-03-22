b99f742e4a3602d96177a84dfffeb3baa96b9224
zackthedelta <m.s.lee.dk@gmail.com>
2026-03-23 00:00:46 +0800

    init: Add build folder for conf

---
+++ build/add_layer.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+bitbake-layers add-layer ../meta-raspberrypi
+bitbake-layers add-layer ../meta-openembedded/meta-oe
+bitbake-layers add-layer ../meta-openembedded/meta-python
+bitbake-layers add-layer ../meta-openembedded/meta-networking
+bitbake-layers add-layer ../meta-openembedded/meta-webserver
+
