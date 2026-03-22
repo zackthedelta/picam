7c0f1005ea1fe2ed5911d759f6f5961679fa9049
zackthedelta <m.s.lee.dk@gmail.com>
2026-03-22 23:39:08 +0800

    init: Initialize meta-layers of branch scarthgap for raspberry pi 3b

---
+++ prepare_env.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+sudo apt update
+
+sudo apt install -y \
+gawk wget git diffstat unzip texinfo gcc build-essential \
+chrpath socat cpio python3 python3-pip python3-pexpect \
+xz-utils debianutils iputils-ping python3-git \
+python3-jinja2 libegl1-mesa libsdl1.2-dev pylint \
+xterm mesa-common-dev zstd liblz4-tool \
+file locales 
+
+sudo locale-gen en_US.UTF-8
+
+git clone -b scarthgap git://git.yoctoproject.org/poky
+git clone -b scarthgap https://github.com/agherzan/meta-raspberrypi.git
+git clone -b scarthgap https://github.com/openembedded/meta-openembedded.git
