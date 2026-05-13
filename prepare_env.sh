3b54189f4f0be69c93557c327c8815770b6c4e32
mianshiuan-li <m.s.lee.dk@gmail.com>
2026-05-13 08:39:21 +0800

    fix: Unable to locate package libegl1-mesa in Ubuntu24.04

--- prepare_env.sh
+++ prepare_env.sh
@@ -6,7 +6,7 @@ sudo apt install -y \
 gawk wget git diffstat unzip texinfo gcc build-essential \
 chrpath socat cpio python3 python3-pip python3-pexpect \
 xz-utils debianutils iputils-ping python3-git \
-python3-jinja2 libegl1-mesa libsdl1.2-dev pylint \
+python3-jinja2 libsdl1.2-dev pylint \
 xterm mesa-common-dev zstd liblz4-tool \
 file locales 
 
